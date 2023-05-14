006f1d4fb470b47e24e737be237a772753f501a3
lahnstrom <love.ahnstrom@gmail.com>
2023-05-14 11:49:33 +0200

    Calculate desired values

--- IL6_distr_Hermansson.R
+++ IL6_distr_Hermansson.R
@@ -9,12 +9,13 @@ rm(list=ls())
 
 # Require packages
 require(foreign)
+require(tidyverse)
 
 # Read data from files
 Abhimanyu_df <- read.csv2("Abhimanyu_derivation.csv")
 Sothern_df <- read.csv2("Sothern1995IndividualData.csv")
-Imaeda_df <- read.csv2("Imaeda derivation.csv", na.strings = c("", "NA"))
-Imaeda_v_df <- read.csv2("Imaeda validation.csv", na.strings = c("", "NA"))
+Imaeda_df <- read.csv2("Imaeda derivation.csv", na.strings = c("", "NA"), check.names = F, encoding = "UTF-8")
+Imaeda_v_df <- read.csv2("Imaeda validation.csv", na.strings = c("", "NA"), check.names = F, encoding = "UTF-8")
 Wand_df <- read.csv2("Wand (2016) data.csv")
 MIDUS_df <- load("29282-0001-Data.rda")
 IL6MIDUS <- data.frame(da29282.0001$B4BMSDIL6)
@@ -31,10 +32,11 @@ IL6Abhimanyu <- na.omit(IL6Abhimanyu)
 
 # Wand - extract IL-6
 IL6Wand <- data.frame(Wand_df$IL6.d1.4h, Wand_df$IL6.d1.16h, Wand_df$IL6.d2.4h, Wand_df$IL6.d2.16h, Wand_df$IL6.d3.4h, Wand_df$IL6.d3.16h, Wand_df$IL6.d4.4h, Wand_df$IL6.d4.16h)
+IL6Wand_stacked <- data.frame(stack(IL6Wand))[1]
 
 # Imaeda - extract IL-6
-IL6Imaeda <- data.frame(Imaeda_df$Interleukin.6..pg.mL)
-IL6Imaeda_v <- data.frame(Imaeda_v_df$Interleukin.6..pg.mL)
+IL6Imaeda <- data.frame(Imaeda_df$`Interleukin-6, pg/mL`)
+IL6Imaeda_v <- data.frame(Imaeda_v_df$`Interleukin-6, pg/mL`)
 
 # Sothern - re-arrange data into one column and extract IL6
 Sothern_df <- data.frame(stack(Sothern_df))
@@ -43,6 +45,34 @@ IL6Sothern <- data.frame(Sothern_df$values)
 # Lekander - extract IL6 and omit NAs
 IL6Lekander <- data.frame(Lekander$il6m)
 IL6Lekander<- na.omit(IL6Lekander)
+
+# Stack data in a 2 column df 
+IL6_only <- bind_rows(IL6Abhimanyu, IL6Wand_stacked, IL6Imaeda, IL6Imaeda_v, IL6Sothern, IL6Lekander)
+colnames(IL6_only) <- c("Abhimanyu", "Wand", "Imaeda", "Imaeda_v", "Sothern", "Lekander")
+IL6_only_stacked <- data.frame(stack(IL6_only)) %>% filter(!is.na(values))
+
+# Group by dataset, for each group run the model and export csv file, once for dnorm and once for dexp
+IL6_only_stacked %>% 
+  group_by(ind) %>%
+  group_walk(~{
+    test <- fitdist(.x$values, distr = dnorm)
+    model_summary <- summary(test)
+    df <- data.frame(dataset=.y$ind[1], loglike=model_summary$loglik, aic=model_summary$aic, bic=model_summary$bic)
+    write.csv(df, sprintf("output/%s-fitdistr-dnorm.csv", .y$ind[1]), col.names = F)
+    
+    tryCatch({
+      test <- fitdist(.x$values, distr = dexp)
+      model_summary <- summary(test)
+      df <- data.frame(dataset=.y$ind[1], loglike=model_summary$loglik, aic=model_summary$aic, bic=model_summary$bic)
+      write.csv(df, sprintf("output/%s-fitdistr-dexp.csv", .y$ind[1]), col.names = F)
+    }, error=function(err) {
+      return(NA)
+    }, warning=function(war) {
+      return(NA)
+    })
+    
+    plot(test)
+  })
 
 
 # 2. Analyse distributions and create tables
